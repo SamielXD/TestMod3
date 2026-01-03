12208e1d86e3c78bfea24943e8750ce4a96b9042
Samiel <163655207+SamielXD@users.noreply.github.com>
2026-01-03 20:25:06 +0800

    Update NodeEditor.java

--- src/studio/NodeEditor.java
+++ src/studio/NodeEditor.java
@@ -25,7 +25,6 @@ public class NodeEditor extends BaseDialog {
 
         buildUI();
 
-        // Scrollable buttons
         Table buttonTable = new Table();
         buttonTable.defaults().size(150f, 80f);
         
@@ -105,6 +104,11 @@ public class NodeEditor extends BaseDialog {
             dialog.hide();
         }).row();
 
+        dialog.cont.button("CREATE MOD FOLDER (Action)", () -> {
+            canvas.addNode("action", "Create Mod Folder", Color.cyan);
+            dialog.hide();
+        }).row();
+
         dialog.cont.button("IF (Condition)", () -> {
             canvas.addNode("condition", "If", Color.orange);
             dialog.hide();
@@ -126,118 +130,17 @@ public class NodeEditor extends BaseDialog {
         dialog.cont.defaults().size(600f, 80f).pad(10f);
 
         if(node.inputs.size > 0) {
-            // SPECIAL HANDLING FOR SPAWN UNIT NODE
+            // SPAWN UNIT NODE - Special handling
             if(node.label.equals("Spawn Unit")) {
-                // Unit Type
-                Label unitLabel = new Label("Unit Type:");
-                unitLabel.setFontScale(1.5f);
-                dialog.cont.add(unitLabel).left().row();
-                
-                TextField unitField = new TextField(node.inputs.get(0).value);
-                unitField.setStyle(new TextField.TextFieldStyle(unitField.getStyle()));
-                unitField.getStyle().font.getData().setScale(1.5f);
-                dialog.cont.add(unitField).fillX().height(100f).row();
-
-                // Spawn Location Dropdown
-                Label locationLabel = new Label("Spawn Location:");
-                locationLabel.setFontScale(1.5f);
-                dialog.cont.add(locationLabel).left().row();
-
-                Table locationButtons = new Table();
-                locationButtons.defaults().size(180f, 80f).pad(5f);
-                
-                final String[] selectedLocation = {node.inputs.get(1).value};
-                
-                TextButton coreBtn = new TextButton("At Core", Styles.togglet);
-                coreBtn.setChecked(selectedLocation[0].equals("core"));
-                coreBtn.changed(() -> {
-                    selectedLocation[0] = "core";
-                    coreBtn.setChecked(true);
-                });
-                locationButtons.add(coreBtn);
-
-                TextButton coordBtn = new TextButton("At Coordinates", Styles.togglet);
-                coordBtn.setChecked(selectedLocation[0].equals("coordinates"));
-                coordBtn.changed(() -> {
-                    selectedLocation[0] = "coordinates";
-                    coordBtn.setChecked(true);
-                });
-                locationButtons.add(coordBtn);
-
-                TextButton playerBtn = new TextButton("At Player", Styles.togglet);
-                playerBtn.setChecked(selectedLocation[0].equals("player"));
-                playerBtn.changed(() -> {
-                    selectedLocation[0] = "player";
-                    playerBtn.setChecked(true);
-                });
-                locationButtons.add(playerBtn);
-
-                dialog.cont.add(locationButtons).row();
-
-                // X Coordinate
-                Label xLabel = new Label("X Coordinate:");
-                xLabel.setFontScale(1.5f);
-                dialog.cont.add(xLabel).left().row();
-                
-                TextField xField = new TextField(node.inputs.get(2).value);
-                xField.setStyle(new TextField.TextFieldStyle(xField.getStyle()));
-                xField.getStyle().font.getData().setScale(1.5f);
-                dialog.cont.add(xField).fillX().height(100f).row();
-
-                // Y Coordinate
-                Label yLabel = new Label("Y Coordinate:");
-                yLabel.setFontScale(1.5f);
-                dialog.cont.add(yLabel).left().row();
-                
-                TextField yField = new TextField(node.inputs.get(3).value);
-                yField.setStyle(new TextField.TextFieldStyle(yField.getStyle()));
-                yField.getStyle().font.getData().setScale(1.5f);
-                dialog.cont.add(yField).fillX().height(100f).row();
-
-                // Amount
-                Label amountLabel = new Label("Amount:");
-                amountLabel.setFontScale(1.5f);
-                dialog.cont.add(amountLabel).left().row();
-                
-                TextField amountField = new TextField(node.inputs.get(4).value);
-                amountField.setStyle(new TextField.TextFieldStyle(amountField.getStyle()));
-                amountField.getStyle().font.getData().setScale(1.5f);
-                dialog.cont.add(amountField).fillX().height(100f).row();
-
-                // Save button
-                dialog.buttons.button("SAVE", () -> {
-                    node.inputs.get(0).value = unitField.getText();
-                    node.inputs.get(1).value = selectedLocation[0];
-                    node.inputs.get(2).value = xField.getText();
-                    node.inputs.get(3).value = yField.getText();
-                    node.inputs.get(4).value = amountField.getText();
-                    
-                    // Update node.value with all info
-                    node.value = unitField.getText() + "|" + selectedLocation[0] + "|" + 
-                                 xField.getText() + "|" + yField.getText() + "|" + amountField.getText();
-                    
-                    dialog.hide();
-                }).size(300f, 100f);
-
-            } else {
-                // DEFAULT HANDLING FOR OTHER NODES
-                for(Node.NodeInput input : node.inputs) {
-                    Label label = new Label(input.label + ":");
-                    label.setFontScale(1.5f);
-                    dialog.cont.add(label).left().row();
-
-                    TextField field = new TextField(input.value);
-                    field.setStyle(new TextField.TextFieldStyle(field.getStyle()));
-                    field.getStyle().font.getData().setScale(1.5f);
-                    dialog.cont.add(field).fillX().height(100f).row();
-
-                    field.changed(() -> {
-                        input.value = field.getText();
-                        node.value = field.getText();
-                    });
-                }
-
-                dialog.buttons.button("DONE", dialog::hide).size(300f, 100f);
+                showSpawnUnitDialog(dialog, node);
+            }
+            // CREATE MOD FOLDER NODE - NEW!
+            else if(node.label.equals("Create Mod Folder")) {
+                showCreateModDialog(dialog, node);
+            }
+            // DEFAULT - All other nodes
+            else {
+                showDefaultEditDialog(dialog, node);
             }
         } else {
             Label label = new Label("This node has no editable properties");
@@ -247,6 +150,162 @@ public class NodeEditor extends BaseDialog {
         }
 
         dialog.show();
+    }
+
+    private void showSpawnUnitDialog(BaseDialog dialog, Node node) {
+        Label unitLabel = new Label("Unit Type:");
+        unitLabel.setFontScale(1.5f);
+        dialog.cont.add(unitLabel).left().row();
+        
+        TextField unitField = new TextField(node.inputs.get(0).value);
+        unitField.setStyle(new TextField.TextFieldStyle(unitField.getStyle()));
+        unitField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(unitField).fillX().height(100f).row();
+
+        Label locationLabel = new Label("Spawn Location:");
+        locationLabel.setFontScale(1.5f);
+        dialog.cont.add(locationLabel).left().row();
+
+        Table locationButtons = new Table();
+        locationButtons.defaults().size(180f, 80f).pad(5f);
+        
+        final String[] selectedLocation = {node.inputs.get(1).value};
+        
+        TextButton coreBtn = new TextButton("At Core", Styles.togglet);
+        coreBtn.setChecked(selectedLocation[0].equals("core"));
+        coreBtn.changed(() -> {
+            selectedLocation[0] = "core";
+            coreBtn.setChecked(true);
+        });
+        locationButtons.add(coreBtn);
+
+        TextButton coordBtn = new TextButton("At Coordinates", Styles.togglet);
+        coordBtn.setChecked(selectedLocation[0].equals("coordinates"));
+        coordBtn.changed(() -> {
+            selectedLocation[0] = "coordinates";
+            coordBtn.setChecked(true);
+        });
+        locationButtons.add(coordBtn);
+
+        TextButton playerBtn = new TextButton("At Player", Styles.togglet);
+        playerBtn.setChecked(selectedLocation[0].equals("player"));
+        playerBtn.changed(() -> {
+            selectedLocation[0] = "player";
+            playerBtn.setChecked(true);
+        });
+        locationButtons.add(playerBtn);
+
+        dialog.cont.add(locationButtons).row();
+
+        Label xLabel = new Label("X Coordinate:");
+        xLabel.setFontScale(1.5f);
+        dialog.cont.add(xLabel).left().row();
+        
+        TextField xField = new TextField(node.inputs.get(2).value);
+        xField.setStyle(new TextField.TextFieldStyle(xField.getStyle()));
+        xField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(xField).fillX().height(100f).row();
+
+        Label yLabel = new Label("Y Coordinate:");
+        yLabel.setFontScale(1.5f);
+        dialog.cont.add(yLabel).left().row();
+        
+        TextField yField = new TextField(node.inputs.get(3).value);
+        yField.setStyle(new TextField.TextFieldStyle(yField.getStyle()));
+        yField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(yField).fillX().height(100f).row();
+
+        Label amountLabel = new Label("Amount:");
+        amountLabel.setFontScale(1.5f);
+        dialog.cont.add(amountLabel).left().row();
+        
+        TextField amountField = new TextField(node.inputs.get(4).value);
+        amountField.setStyle(new TextField.TextFieldStyle(amountField.getStyle()));
+        amountField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(amountField).fillX().height(100f).row();
+
+        dialog.buttons.button("SAVE", () -> {
+            node.inputs.get(0).value = unitField.getText();
+            node.inputs.get(1).value = selectedLocation[0];
+            node.inputs.get(2).value = xField.getText();
+            node.inputs.get(3).value = yField.getText();
+            node.inputs.get(4).value = amountField.getText();
+            
+            node.value = unitField.getText() + "|" + selectedLocation[0] + "|" + 
+                         xField.getText() + "|" + yField.getText() + "|" + amountField.getText();
+            
+            dialog.hide();
+        }).size(300f, 100f);
+    }
+
+    private void showCreateModDialog(BaseDialog dialog, Node node) {
+        Label nameLabel = new Label("Mod Folder Name:");
+        nameLabel.setFontScale(1.5f);
+        dialog.cont.add(nameLabel).left().row();
+        
+        TextField nameField = new TextField(node.inputs.get(0).value);
+        nameField.setStyle(new TextField.TextFieldStyle(nameField.getStyle()));
+        nameField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(nameField).fillX().height(100f).row();
+
+        Label displayNameLabel = new Label("Display Name:");
+        displayNameLabel.setFontScale(1.5f);
+        dialog.cont.add(displayNameLabel).left().row();
+        
+        TextField displayNameField = new TextField(node.inputs.get(1).value);
+        displayNameField.setStyle(new TextField.TextFieldStyle(displayNameField.getStyle()));
+        displayNameField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(displayNameField).fillX().height(100f).row();
+
+        Label authorLabel = new Label("Author:");
+        authorLabel.setFontScale(1.5f);
+        dialog.cont.add(authorLabel).left().row();
+        
+        TextField authorField = new TextField(node.inputs.get(2).value);
+        authorField.setStyle(new TextField.TextFieldStyle(authorField.getStyle()));
+        authorField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(authorField).fillX().height(100f).row();
+
+        Label descLabel = new Label("Description:");
+        descLabel.setFontScale(1.5f);
+        dialog.cont.add(descLabel).left().row();
+        
+        TextField descField = new TextField(node.inputs.get(3).value);
+        descField.setStyle(new TextField.TextFieldStyle(descField.getStyle()));
+        descField.getStyle().font.getData().setScale(1.5f);
+        dialog.cont.add(descField).fillX().height(100f).row();
+
+        dialog.buttons.button("SAVE", () -> {
+            node.inputs.get(0).value = nameField.getText();
+            node.inputs.get(1).value = displayNameField.getText();
+            node.inputs.get(2).value = authorField.getText();
+            node.inputs.get(3).value = descField.getText();
+            
+            node.value = nameField.getText() + "|" + displayNameField.getText() + "|" + 
+                         authorField.getText() + "|" + descField.getText();
+            
+            dialog.hide();
+        }).size(300f, 100f);
+    }
+
+    private void showDefaultEditDialog(BaseDialog dialog, Node node) {
+        for(Node.NodeInput input : node.inputs) {
+            Label label = new Label(input.label + ":");
+            label.setFontScale(1.5f);
+            dialog.cont.add(label).left().row();
+
+            TextField field = new TextField(input.value);
+            field.setStyle(new TextField.TextFieldStyle(field.getStyle()));
+            field.getStyle().font.getData().setScale(1.5f);
+            dialog.cont.add(field).fillX().height(100f).row();
+
+            field.changed(() -> {
+                input.value = field.getText();
+                node.value = field.getText();
+            });
+        }
+
+        dialog.buttons.button("DONE", dialog::hide).size(300f, 100f);
     }
 
     private void saveScript() {
@@ -276,7 +335,6 @@ public class NodeEditor extends BaseDialog {
                     data.value = node.value;
                     data.color = node.color.toString();
 
-                    // Save all input values
                     data.inputValues = new Seq<>();
                     for(Node.NodeInput input : node.inputs) {
                         data.inputValues.add(input.value);
@@ -299,6 +357,7 @@ public class NodeEditor extends BaseDialog {
                 dialog.hide();
             } catch(Exception e) {
                 Log.err("Save failed", e);
+                Vars.ui.showInfoFade("Save failed: " + e.getMessage());
             }
         }).size(250f, 100f);
 
@@ -337,12 +396,20 @@ public class NodeEditor extends BaseDialog {
     private void loadScript(String name) {
         try {
             String json = Core.files.local("mods/studio-scripts/" + name + ".json").readString();
-            Seq<NodeData> nodeDataList = new Json().fromJson(Seq.class, NodeData.class, json);
+            
+            // FIXED: Proper JSON deserialization
+            Json jsonParser = new Json();
+            jsonParser.setIgnoreUnknownFields(true);
+            Seq<NodeData> nodeDataList = jsonParser.fromJson(Seq.class, NodeData.class, json);
+
+            if(nodeDataList == null || nodeDataList.size == 0) {
+                throw new Exception("No nodes in file");
+            }
 
             canvas.nodes.clear();
             Seq<Node> loadedNodes = new Seq<>();
 
-            // FIXED: Properly load nodes with all data
+            // Load nodes
             for(NodeData data : nodeDataList) {
                 Node node = new Node();
                 node.id = data.id;
@@ -350,13 +417,13 @@ public class NodeEditor extends BaseDialog {
                 node.label = data.label;
                 node.x = data.x;
                 node.y = data.y;
-                node.value = data.value;
+                node.value = data.value != null ? data.value : "";
                 node.color = Color.valueOf(data.color);
                 node.width = 400f;
                 node.height = 200f;
                 node.setupInputs();
 
-                // FIXED: Restore input values
+                // FIXED: Restore input values properly
                 if(data.inputValues != null && data.inputValues.size > 0) {
                     for(int i = 0; i < Math.min(node.inputs.size, data.inputValues.size); i++) {
                         node.inputs.get(i).value = data.inputValues.get(i);
@@ -364,6 +431,7 @@ public class NodeEditor extends BaseDialog {
                 }
 
                 loadedNodes.add(node);
+                Log.info("Loaded node: " + node.label + " at (" + node.x + ", " + node.y + ")");
             }
 
             // Restore connections
@@ -371,11 +439,12 @@ public class NodeEditor extends BaseDialog {
                 NodeData data = nodeDataList.get(i);
                 Node node = loadedNodes.get(i);
 
-                if(data.connectionIds != null) {
+                if(data.connectionIds != null && data.connectionIds.size > 0) {
                     for(String connId : data.connectionIds) {
                         Node target = loadedNodes.find(n -> n.id.equals(connId));
                         if(target != null) {
                             node.connections.add(target);
+                            Log.info("Connected: " + node.label + " -> " + target.label);
                         }
                     }
                 }
@@ -384,10 +453,12 @@ public class NodeEditor extends BaseDialog {
             canvas.nodes = loadedNodes;
             currentScriptName = name;
             statusLabel.setText("Loaded: " + name + " (" + canvas.nodes.size + " nodes)");
+            Vars.ui.showInfoFade("Loaded " + canvas.nodes.size + " nodes!");
 
         } catch(Exception e) {
-            Log.err("Load failed", e);
-            Vars.ui.showInfoFade("Failed to load: " + e.getMessage());
+            Log.err("Load failed: " + name, e);
+            Vars.ui.showInfoFade("Load failed: " + e.getMessage());
+            statusLabel.setText("Load FAILED!");
         }
     }
 
@@ -426,6 +497,6 @@ public class NodeEditor extends BaseDialog {
         public String value;
         public String color;
         public Seq<String> connectionIds = new Seq<>();
-        public Seq<String> inputValues = new Seq<>(); // NEW: Save all input values
+        public Seq<String> inputValues = new Seq<>();
     }
 }
